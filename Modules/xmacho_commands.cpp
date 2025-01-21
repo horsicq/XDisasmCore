e37fcc158f13c226895e88527e3cd8e1de3da0a3
horsicq <horsicq@gmail.com>
2025-01-21 08:20:03 +0100

    Add new file(s): 2025-01-21

---
+++ Modules/xmacho_commands.cpp
@@ -0,0 +1,72 @@
+/* Copyright (c) 2025 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "xmacho_commands.h"
+
+XMachO_Commands::XMachO_Commands(XBinary::DM disasmMode, QObject *parent)
+    : XDisasmAbstract(parent)
+{
+    g_disasmMode = disasmMode;
+}
+
+quint64 XMachO_Commands::_handleULEB128(QList<DISASM_RESULT> *pListResults, char *pData, STATE *pState, const DISASM_OPTIONS &disasmOptions, QString sPrefix)
+{
+    if (pState->bIsStop) {
+        return 0;
+    }
+
+    quint64 nResult = 0;
+
+    XBinary::PACKED_UINT puTag = XBinary::_read_uleb128(pData + pState->nCurrentOffset, pState->nMaxSize - pState->nCurrentOffset);
+
+    if (puTag.bIsValid) {
+        nResult = puTag.nValue;
+        _addDisasmResult(pListResults, pState->nAddress + pState->nCurrentOffset, puTag.nByteSize, sPrefix, QString("0x%1").arg(QString::number(puTag.nValue, 16)),
+                         pState, disasmOptions);
+    } else {
+        pState->bIsStop = true;
+    }
+
+    return nResult;
+}
+
+QList<XDisasmAbstract::DISASM_RESULT> XMachO_Commands::_disasm(char *pData, qint32 nDataSize, XADDR nAddress, const DISASM_OPTIONS &disasmOptions, qint32 nLimit, XBinary::PDSTRUCT *pPdStruct)
+{
+    QList<XDisasmAbstract::DISASM_RESULT> listResult;
+
+    STATE state = {};
+    state.nCurrentCount = 0;
+    state.nCurrentOffset = 0;
+    state.nLimit = nLimit;
+    state.nMaxSize = nDataSize;
+    state.nAddress = nAddress;
+
+    if (g_disasmMode == XBinary::DM_CUSTOM_MACH_EXPORT) {
+        while (!(state.bIsStop)) {
+            _handleULEB128(&listResult, pData, &state, disasmOptions, "TERMINAL_SIZE");
+            _handleULEB128(&listResult, pData, &state, disasmOptions, "FLAGS");
+        }
+    } else {
+        _addDisasmResult(&listResult, nAddress, nDataSize, "ARRAY", "TST", &state, disasmOptions);
+    }
+
+    return listResult;
+}
---
+++ Modules/xmacho_commands.h
@@ -0,0 +1,45 @@
+/* Copyright (c) 2025 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef XMACHO_COMMANDS_H
+#define XMACHO_COMMANDS_H
+
+#include "../xdisasmabstract.h"
+#include "xmach.h"
+
+class XMachO_Commands : public XDisasmAbstract
+{
+    Q_OBJECT
+
+public:
+    explicit XMachO_Commands(XBinary::DM disasmMode, QObject *parent = nullptr);
+
+    virtual QList<DISASM_RESULT> _disasm(char *pData, qint32 nDataSize, XADDR nAddress, const XDisasmAbstract::DISASM_OPTIONS &disasmOptions, qint32 nLimit,
+                                         XBinary::PDSTRUCT *pPdStruct);
+
+private:
+    quint64 _handleULEB128(QList<DISASM_RESULT> *pListResults, char *pData, STATE *pState, const XDisasmAbstract::DISASM_OPTIONS &disasmOptions, QString sPrefix);
+
+private:
+    XBinary::DM g_disasmMode;
+};
+
+#endif // XMACHO_COMMANDS_H
